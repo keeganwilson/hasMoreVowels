80bb3f5facf3d346989f91ed055da82d06f1e809
Keegan Wilson <keegan.wilson@gmail.com>
2022-04-03 12:07:40 -0600

    completed

---
+++ hasMoreVowels.js
@@ -0,0 +1,19 @@
+const hasMoreVowels = word => {
+    word = word.toLowerCase();
+    let vowels = ['a', 'e', 'i', 'o', 'u']
+    let howManyVowels = 0
+    for (i = 0; i < word.length; i++) {
+        if (vowels.includes(word[i])) {
+            howManyVowels++
+        }
+    }
+    if (word.length - howManyVowels >= word.length / 2) {
+        return false
+    }   else return true
+}
+
+console.log(hasMoreVowels('moose'));
+console.log(hasMoreVowels('mice'));
+console.log(hasMoreVowels('graph'));
+console.log(hasMoreVowels('yay'));
+console.log(hasMoreVowels('Aal'));
